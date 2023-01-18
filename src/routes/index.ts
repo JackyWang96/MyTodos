99f438ee41e263913200a446ce463b77a54f7c9a
Jackywang96 <jackywangmel96@gmail.com>
2023-01-18 16:31:19 +1100

    create routes

---
+++ src/routes/index.ts
@@ -0,0 +1,7 @@
+import { Router } from "express"
+import { getTodos, addTodo, updateTodo, deleteTodo } from "../controllers/todos"
+const router: Router = Router()
+router.get("/todos", getTodos)
+router.post("/add-todo", addTodo)
+router.put("/edit-todo/:id", updateTodo)
+router.delete("/delete-todo/:id", deleteTodo)
